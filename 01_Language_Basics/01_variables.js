64d02d49173187c4df47c7af002374d22c3c9473
keal khondekar <kewalkhondekar25@gmail.com>
2023-08-13 21:12:49 +0530

    🖥️

--- 01_Language_Basics/01_variables.js
+++ 01_Language_Basics/01_variables.js
@@ -12,4 +12,37 @@ var userName = 'arjun';
 console.log(userName);
 
 
+// var is function scope variable
+function f1(){
+    var x;
+    x = 10;
+
+    if(x == 10){
+        var y = 20;
+    }
+    console.table([x,y]);
+}
+f1();
+
+//var allows shadowing
+function f2(){
+    var a = 101;
+
+    if(a == 101){
+        var b = 102;
+        //console.log(b);
+        var b = 103;
+        console.log(b);
+    }
+}
+f2();
+
+//var also allow hoisting in strict mode
+// 'use strict'
+function f3(){
+    a = 70;
+    console.log(a);
+    var a; //hosting
+}
+f3();
 
