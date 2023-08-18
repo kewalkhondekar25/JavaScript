0071589d0a636a0d2cf0a475b751f286ee829ef6
keal khondekar <kewalkhondekar25@gmail.com>
2023-08-18 15:02:34 +0530

    feat: data types

---
+++ 02_Data_Types/Number_Types/2_number.js
@@ -0,0 +1,24 @@
+/*
+
+var a = 10AB;
+var b = 20;
+
+var c = parseInt(a) + b   //30 
+
+
+var x = AB10;
+var y = 20;
+
+var z = parseInt(x) + y   //INVALID
+
+var k = 10AB20;           //10
+var l = 10;
+
+var m = parseInt(k) + l   //20
+
+var num1 = 10;
+var num2 = true;
+
+var result = num1 + num2   //11
+
+*/
