f6838a90ceebfe3dd685366199218a648c7d6145
keal khondekar <kewalkhondekar25@gmail.com>
2024-10-18 17:56:40 +0530

    feat: data types added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treats all js code as version
+
+let name = "john";
+let age = 26;
+let isAdult = true;
+let status;
+
+console.table([name, age, isAdult, status]);
+/*
+┌─────────┬───────────┐
+│ (index) │ Values    │
+├─────────┼───────────┤
+│ 0       │ 'john'    │
+│ 1       │ 26        │
+│ 2       │ true      │
+│ 3       │ undefined │
+└─────────┴───────────┘
+*/
