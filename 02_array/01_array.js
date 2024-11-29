ebf0a8e7f2613a12192e698e1e6e9d6e15d82c0c
keal khondekar <kewalkhondekar25@gmail.com>
2024-11-30 00:50:24 +0530

    feat: array basics added

---
+++ 02_array/01_array.js
@@ -0,0 +1,9 @@
+const arr = [0, 1, 2, 3, 4, 5];
+for(let i = 0; i < arr.length; i++){
+  console.log(arr[i]);
+};
+
+const newArr = new Array(10, 20, 30, 40, 50);
+for(let i = 0; i < newArr.length; i++){
+  console.log(newArr[i]);
+};
