bc8a887876fa17befc063fc44379a0134d87de2d
keal khondekar <kewalkhondekar25@gmail.com>
2024-12-09 11:04:10 +0530

    feat: object destructuring added

---
+++ 03_objects/03_objects.js
@@ -0,0 +1,11 @@
+//destructuring
+
+const userData = {
+  id: 69,
+  name: "john doe",
+  email: "johndoe.gmail.com",
+  age: 26
+}
+
+const { id, name, email } = userData;
+console.log("id: ", id);
